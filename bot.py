3b9253b8fd3c8cb9b1a45b09d95f72edc3f25b3a
webabrakadabra <hireshnik@gmail.com>
2021-05-19 15:14:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-#chrome_options = Options()
-#chrome_options.add_argument('--headless')
 path = '/home/aster/bot_selenium_chrome_90/chromedriver'
 browser = webdriver.Chrome(path)
 browser.get('http://google.com')
