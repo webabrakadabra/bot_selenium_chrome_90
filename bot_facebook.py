f75c0abd1d27412b11c85a963353a9606fd30961
webabrakadabra <hireshnik@gmail.com>
2021-06-25 15:11:53 +0300

    a

--- bot_facebook.py
+++ bot_facebook.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
 
 path = '/home/aster/bot_selenium_chrome_90/chromedriver'
 options = webdriver.ChromeOptions()
@@ -16,6 +18,12 @@ submit = browser.find_element_by_name('login')
 email.send_keys('0668237072')
 password.send_keys('3152418512_aA')
 submit.click()
+time.sleep(10)
+browser.get('https://www.facebook.com/friends/list')
+time.sleep(10)
+page = browser.find_element_by_xpath("//div[@aria-label='Усі друзі']")
+#page = browser.find_elements_by_css_selector("[aria-label=Усі друзі]")
+page.send_keys(Keys.END)
 
 #закриття браузера
 ####################################################
